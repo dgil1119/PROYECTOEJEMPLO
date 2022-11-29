56db731e484bdcc4d01fd24f33d3fea5fd84cd32
tunick <dgil1119@ceuandalucia.es>
2022-11-29 19:46:31 +0100

    OUIwhfeu

--- src/array20/array20.java
+++ src/array20/array20.java
@@ -1,5 +1,23 @@
 package array20;
 
-public class array20 {
+import java.util.Random;
+import java.util.Scanner;
 
+public class array20 {
+public static void main(String[] args) {
+	Scanner scanner= new Scanner(System.in);
+	Random random = new Random();
+
+	String palabras[]=new String[] { "azulejo", "tenedor", "saltamontes",
+			"carretilla", "molinero", "sofisticado", "terremoto", "culinario", "teclado", "primavera"};
+	Integer aleatorio = random.nextInt(10);
+	String palabra1=aleatorio.toString();
+	for(int i=0; i<palabras.length-1;i++) {
+		if(palabras[i].equals(aleatorio)) {
+			
+		}
+	}
+	
+	scanner.close();
+}
 }
